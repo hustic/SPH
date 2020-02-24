8bdd7b53a368af2b3b72bee9e29b264107d0c58c
acse-wy619 <56033652+acse-wy619@users.noreply.github.com>
2020-02-24 16:21:51 +0000

    Add files via upload

--- includes/SPH_2D.h
+++ includes/SPH_2D.h
@@ -1,4 +1,6 @@
+
 #pragma once
+#define _USE_MATH_DEFINES
 #include <vector>
 #include <cmath>
 #include <iostream>
@@ -12,8 +14,9 @@ class SPH_particle
 public:
 	double x[2], v[2];				//position and velocity
 	double rho, P;					//density and pressure
+	double a[2], D;					//acceleration and rate of change of density
 
-	static SPH_main *main_data;		//link to SPH_main class so that it can be used in calc_index
+	static SPH_main* main_data;		//link to SPH_main class so that it can be used in calc_index
 
 	int list_num[2];				//index in neighbour finding array
 
@@ -25,19 +28,25 @@ class SPH_main
 {
 public:
 	SPH_main();
+	double cubic_spline(double r[2]);
+	double cubic_spline_first_derivative(double r[2]);
+	void update_gradients(double r[2], SPH_particle* part, SPH_particle* other_part);
 
 	void set_values(void);
 	void initialise_grid(void);
 
-	void place_points(double *min, double *max);
+	void place_points(double* min, double* max);
 
 	void allocate_to_grid(void);			//allocates all the points to the search grid (assumes that index has been appropriately updated)
 
-	void neighbour_iterate(SPH_particle *part);
+	void neighbour_iterate(SPH_particle* part);
 
 	double h;								//smoothing length
 	double h_fac;
 	double dx;								//particle initial spacing
+
+	double g[2];							//gravity constant
+	double mu;								//viscosity
 
 	double min_x[2], max_x[2];				//dimensions of simulation region
 
