80a6c79461ee1264637091d8854e2930113446e0
acse-wy619 <56033652+acse-wy619@users.noreply.github.com>
2020-02-25 13:58:47 +0000

    Add files via upload

--- src/SPH_2D.cpp
+++ src/SPH_2D.cpp
@@ -1,8 +1,7 @@
 ﻿#include "SPH_2D.h"
 
 
-
-SPH_main *SPH_particle::main_data;
+SPH_main* SPH_particle::main_data;
 
 void SPH_particle::calc_index(void)
 {
@@ -14,6 +13,8 @@ SPH_main::SPH_main()
 {
 	SPH_particle::main_data = this;
 }
+
+
 double SPH_main::cubic_spline(double r[2])
 {
 	double q = sqrt(r[0] * r[0] + r[1] * r[1]) / h;
@@ -23,7 +24,7 @@ double SPH_main::cubic_spline(double r[2])
 	}
 	else if (q > 1 and q <= 2)
 	{
-		return 10 * 0.25 * pow((2 - q), 3) / (7 * M_PI * h * h);
+		return -10 * 0.25 * pow((2 - q), 3) / (7 * M_PI * h * h);
 	}
 	else { return 0; }
 }
@@ -42,60 +43,51 @@ double SPH_main::cubic_spline_first_derivative(double r[2])
 	else { return 0; }
 }
 
-void SPH_main::update_gradients(double r[2], SPH_particle* part, SPH_particle* other_part)		//updates acceleration and rate of change of density
+void SPH_main::update_gradients(double r[2], SPH_particle* part, SPH_particle* other_part)		//updates acceleration and rate of change of density of a particle
 {
 	double mass = part->rho * dx * dx;
 	double vij[2], eij[2];
 	double dwdr = cubic_spline_first_derivative(r);
-	part->D += mass * dwdr * (vij[0] * eij[0] + vij[1] * eij[1]);
 	for (int n = 0; n < 2; n++)
 	{
 		vij[n] = part->v[n] - other_part->v[n];
 		eij[n] = r[n] / sqrt(r[0] * r[0] + r[1] * r[1]);
-		part->a[n] += -mass * (part->P / (part->rho * part->rho) + other_part->P / (other_part->rho * other_part->rho)) * dwdr * eij[n] + mu * mass * (1 / (part->rho * part->rho) + 1 / (other_part->rho * other_part->rho)) * dwdr * vij[n] / sqrt(r[0] * r[0] + r[1] * r[1]) + g[n];
+		part->a[n] += -mass * (part->P / (part->rho * part->rho) + other_part->P / (other_part->rho * other_part->rho)) * dwdr * eij[n] + mu * mass * (1 / (part->rho * part->rho) + 1 / (other_part->rho * other_part->rho)) * dwdr * vij[n] / sqrt(r[0] * r[0] + r[1] * r[1]);
 	}
-}
-
-double SPH_main::cubic_spline(double r[2])
-{
-	double q = sqrt(r[0] * r[0] + r[1] * r[1]) / h;
-	if (q >= 0 && q <= 1)
-	{
-		return 10 * (1 - 1.5 * q * q + 0.75 * pow(q, 3)) / (7 * M_PI * h * h);
-	}
-	else if (q > 1 and q <= 2)
-	{
-		return 10  * 0.25 * pow((2 - q), 3) / (7 * M_PI * h * h);
-	}
-	else { return 0; }
-}
-
-double SPH_main::cubic_spline_first_derivative(double r[2])
-{
-	double q = sqrt(r[0] * r[0] + r[1] * r[1]) / h;
-	if (q >= 0 && q <= 1)
-	{
-		return 10 * (-3 * q + 2.25 * q * q) / (7 * M_PI * pow(h, 3));
-	}
-	else if (q > 1 and q <= 2)
-	{
-		return 10 * 0.75 * (2 - q) * (2 - q) / (7 * M_PI * pow(h, 3));
-	}
-	else { return 0; }
-}
-
-void SPH_main::update_gradients(double r[2], SPH_particle* part, SPH_particle* other_part)		//updates acceleration and rate of change of density
-{	
-	double mass = part->rho * dx * dx;
-	double vij[2], eij[2];
-	double dwdr = cubic_spline_first_derivative(r);
 	part->D += mass * dwdr * (vij[0] * eij[0] + vij[1] * eij[1]);
-	for (int n = 0; n < 2; n++)
-	{
-		vij[n] = part->v[n] - other_part->v[n];
-		eij[n] = r[n] / sqrt(r[0] * r[0] + r[1] * r[1]);
-		part->a[n] += -mass * (part->P / (part->rho * part->rho) + other_part->P / (other_part->rho * other_part->rho)) * dwdr * eij[n] + mu * mass * (1 / (part->rho * part->rho) + 1 / (other_part->rho * other_part->rho)) * dwdr * vij[n] / sqrt(r[0] * r[0] + r[1] * r[1]) + g[n];
-	}
+}
+
+void SPH_main::density_field_smoothing(SPH_particle* part)		//performs the density field smoothing for a particle
+{
+	SPH_particle* other_part;
+	double dist;			//distance between particles
+	double dn[2];			//vector from 1st to 2nd particle
+	double numerator = 0.0;
+	double denominator = 0.0;
+
+	for (int i = part->list_num[0] - 1; i <= part->list_num[0] + 1; i++)
+		if (i >= 0 && i < max_list[0])
+			for (int j = part->list_num[1] - 1; j <= part->list_num[1] + 1; j++)
+				if (j >= 0 && j < max_list[1])
+				{
+					for (unsigned int cnt = 0; cnt < search_grid[i][j].size(); cnt++)
+					{
+						other_part = search_grid[i][j][cnt];
+
+						//Calculates the distance between potential neighbours
+						for (int n = 0; n < 2; n++)
+							dn[n] = part->x[n] - other_part->x[n];
+
+						dist = sqrt(dn[0] * dn[0] + dn[1] * dn[1]);
+
+						if (dist < 2. * h)					//only particle within 2h
+						{
+							numerator += cubic_spline(dn);
+							denominator += numerator / other_part->rho;
+						}
+					}
+				}
+	part->rho = numerator / denominator;
 }
 
 void SPH_main::set_values(void)
@@ -107,14 +99,21 @@ void SPH_main::set_values(void)
 	max_x[1] = 1.0;
 
 	dx = 0.02;
+	c0 = 20;
 
 	mu = 0.001;
 	g[0] = 0.0;
 	g[1] = 9.81;
-	
+	rho0 = 1000;
 
 	h_fac = 1.3;
+
 	h = dx * h_fac;
+	dt = 0.1 * h / c0;
+
+	gamma = 7.0;
+	B = c0 * c0 * rho0 / gamma;
+
 }
 
 void SPH_main::initialise_grid(void)
@@ -144,7 +143,14 @@ void SPH_main::place_points(double* min, double* max)
 		while (x[1] <= max[1])
 		{
 			for (int i = 0; i < 2; i++)
+			{
 				particle.x[i] = x[i];
+				particle.a[i] = 0.0 + g[i];
+				particle.v[i] = 0.0;
+			}
+			particle.D = 0.0;
+			particle.rho = rho0;
+			particle.P = 0.0;
 
 			particle.calc_index();
 
@@ -176,13 +182,12 @@ void SPH_main::neighbour_iterate(SPH_particle* part)					//iterates over all par
 	SPH_particle* other_part;
 	double dist;			//distance between particles
 	double dn[2];			//vector from 1st to 2nd particle
-	double tmax = 30;
-	double t = 0;
-	double dt = 0.04;
-	int y = 7;
-	int c0 = 20;
-	double rho0 = 1000;
-	double B = (rho0 * c0 * c0) / y;
+
+	for (int n = 0; n < 2; n++)
+	{
+		part->a[n] = 0.0 + g[n];
+	}
+	part->D = 0.0;
 
 	for (int i = part->list_num[0] - 1; i <= part->list_num[0] + 1; i++)
 		if (i >= 0 && i < max_list[0])
@@ -201,26 +206,42 @@ void SPH_main::neighbour_iterate(SPH_particle* part)					//iterates over all par
 
 							dist = sqrt(dn[0] * dn[0] + dn[1] * dn[1]);
 							if (dist < 2. * h)					//only particle within 2h
-							{
-								
-								cout << "dn: " << dn[0] << " " << dn[1] << endl;		//Should be removed from the code - simply here for you to see that it is working
-							}
+								update_gradients(dn, part, other_part);
 						}
 					}
 				}
+}
 
 
-	//TODO: all the interactions between the particles
-	while (t < tmax)
+void SPH_main::update_particle()
+{
+	vector<SPH_particle> half_particle_list = particle_list;
+
+	int num = half_particle_list.size();
+
+	for (int i = 0; i < num; i++)
 	{
-		//if()
 		for (int k = 0; k < 2; k++)
 		{
-			part->v[k] = part->v[k] + dt * part->a[k];
-			part->x[k] = part->x[k] + dt * part->v[k];
+			half_particle_list[i].x[k] = particle_list[i].x[k] + 0.5 * dt * particle_list[i].v[k];
+			half_particle_list[i].v[k] = particle_list[i].v[k] + 0.5 * dt * particle_list[i].a[k];
 		}
-		part->rho = part->rho * part->D;
-		part->P = B *(pow((part->rho/rho0),y)-1) ;
-		t = t + dt;
+		half_particle_list[i].rho = particle_list[i].rho + 0.5 * dt * particle_list[i].D;
+	}
+
+	for (int i = 0; i < num; i++)
+	{
+		neighbour_iterate(&half_particle_list[i]);
+	}
+
+	for (int i = 0; i < num; i++)
+	{
+		for (int k = 0; k < 2; k++)
+		{
+			particle_list[i].x[k] = particle_list[i].x[k] + dt * half_particle_list[i].v[k];
+			particle_list[i].v[k] = particle_list[i].v[k] + dt * half_particle_list[i].a[k];
+		}
+		particle_list[i].rho = particle_list[i].rho + dt * half_particle_list[i].D;
+		particle_list[i].P = B * (pow((particle_list[i].rho / rho0), gamma) - 1);
 	}
 }
