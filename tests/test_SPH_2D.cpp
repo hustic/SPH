eada6d8a89c777780e09128177856dc092ac9665
acse-sg3219 <sotiris.gkoulimaris19@imperial.ac.uk>
2020-02-25 17:50:14 +0000

    include BOOST tests

--- tests/test_SPH_2D.cpp
+++ tests/test_SPH_2D.cpp
@@ -1,18 +1,36 @@
+#define BOOST_TEST_MODULE test SPH_2D
+#include <boost/test/included/unit_test.hpp>
+
 #include "SPH_2D.h"
 #include "file_writer.h"
 
+using namespace boost::unit_test;
+
 SPH_main domain;
 
-int main(void)
-{
-	domain.set_values();										//Set simulation parameters
-	domain.initialise_grid();									//initialise simulation grid
+int values[] = {};
 
-	domain.place_points(domain.min_x,domain.max_x);				//places initial points - will need to be modified to include boundary points and the specifics of where the fluid is in the domain
+
+BOOST_AUTO_TEST_CASE(TestDomainValues)
+{
+    domain.set_values();										//Set simulation parameters
+	
+    BOOST_TEST(true);
+}
+
+BOOST_AUTO_TEST_CASE(Test_initialise_grid)
+{
+    domain.initialise_grid();									//initialise simulation grid
+    BOOST_TEST(domain.search_grid.size() == domain.max_list[0]);
+    for (int i = 0; i < domain.max_list[0]; i++)
+        BOOST_TEST(domain.search_grid[i].size() == domain.max_list[1]);
+}
+
+BOOST_AUTO_TEST_CASE(TestStuff)
+{
+    domain.place_points(domain.min_x,domain.max_x);				//places initial points - will need to be modified to include boundary points and the specifics of where the fluid is in the domain
 
 	domain.allocate_to_grid();									//needs to be called for each time step
 
 	domain.neighbour_iterate(&domain.particle_list[100]);		//finding all the neighbours of the 100th particle in the list - in reality the simulation loop will need to do the calculations for the neighbours of every particle
-	
-	return 0;
 }
