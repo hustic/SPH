0179805b1d0627206c5bb2ed67e152d7380e0724
acse-wy619 <56033652+acse-wy619@users.noreply.github.com>
2020-02-25 13:59:14 +0000

    Add files via upload

--- src/SPH_Snippet.cpp
+++ src/SPH_Snippet.cpp
@@ -1,5 +1,9 @@
 #include "SPH_2D.h"
 #include "file_writer.h"
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
 
 SPH_main domain;
 
@@ -8,13 +12,32 @@ int main(void)
 	domain.set_values();										//Set simulation parameters
 	domain.initialise_grid();									//initialise simulation grid
 
-	domain.place_points(domain.min_x,domain.max_x);				//places initial points - will need to be modified to include boundary points and the specifics of where the fluid is in the domain
+	domain.place_points(domain.min_x, domain.max_x);				//places initial points - will need to be modified to include boundary points and the specifics of where the fluid is in the domain
 
 	domain.allocate_to_grid();									//needs to be called for each time step
 
-	domain.neighbour_iterate(&domain.particle_list[100]);		//finding all the neighbours of the 100th particle in the list - in reality the simulation loop will need to do the calculations for the neighbours of every particle
+	stringstream name;
+	name << "initial_configuration.vtp";
 
-	write_file("example.vtp", &domain.particle_list);
-	
+
+	write_file(name.str().c_str(), &domain.particle_list);
+
+
+	for (int iter = 0; iter < 10; iter++) {
+		for (int i = 0; i < domain.particle_list.size(); i++)
+			domain.neighbour_iterate(&domain.particle_list[i]);
+			domain.update_particle();
+
+		if (iter % 10 == 0) {
+			for (int i = 0; i < domain.particle_list.size(); i++)
+				domain.density_field_smoothing(&domain.particle_list[i]);
+		}
+
+
+		stringstream name;
+		name << "output" << "_" << setfill('0') << setw(int(to_string(100).length())) << iter << ".vtp";
+		write_file(name.str().c_str(), &domain.particle_list);
+
+	}
 	return 0;
 }
