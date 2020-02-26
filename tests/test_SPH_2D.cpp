f77501a2f4e646548bdd4646411e66a5cf5c2d68
acse-sg3219 <sotiris.gkoulimaris19@imperial.ac.uk>
2020-02-26 17:13:22 +0000

    added mroe tests

--- tests/test_SPH_2D.cpp
+++ tests/test_SPH_2D.cpp
@@ -12,20 +12,6 @@ namespace tt=boost::test_tools;
 
 SPH_main domain;
 
-
-BOOST_AUTO_TEST_SUITE(DomainConstructor);
-
-BOOST_AUTO_TEST_CASE(TestInitialiseGrid)
-{
-    domain.set_values();
-    domain.initialise_grid();									//initialise simulation grid
-    BOOST_TEST(domain.search_grid.size() == domain.max_list[0]);
-    for (int i = 0; i < domain.max_list[0]; i++)
-        BOOST_TEST(domain.search_grid[i].size() == domain.max_list[1]);
-}
-
-BOOST_AUTO_TEST_SUITE_END();
-
 /* 
  * Spline Functions Tests
  */
@@ -74,7 +60,7 @@ BOOST_AUTO_TEST_CASE(TestCubicSplineFirstGreater, \
     domain.h = 1.;
     double dn[2] = {1, 0};
     double tres = domain.cubic_spline_first_derivative(dn);
-    double res = -10 * 0.75 * (2 - 1) * (2 - 1) / (7 * M_PI);
+    double res = -10 * 0.75 / (7 * M_PI);
     BOOST_TEST(tres == res, tt::tolerance(0.0001));
 }
 
@@ -91,3 +77,143 @@ BOOST_AUTO_TEST_CASE(TestCubicSplineFirstOutside, \
 BOOST_AUTO_TEST_SUITE_END();
 
 
+/*
+ * Gradient Function Tests
+ */
+
+BOOST_AUTO_TEST_SUITE(TestGradient, * description("Testing the Gradient Update Function"));
+
+BOOST_AUTO_TEST_CASE(TestGradientLess, * description("Distance between 0 and 1"))
+{
+    SPH_particle* p1 = new SPH_particle();
+    SPH_particle* p2 = new SPH_particle();
+    
+    domain.h = 1.;
+    domain.mass = 1.;
+    domain.mu = 1.;
+    
+    p1->v[0] = 1.;
+    p1->v[1] = 0.;
+    p1->rho = 1.;
+    p1->P = 1.;
+    p1->a[0] = 0.;
+    p1->a[1] = 0.;
+    p1->D = 0.;
+    
+    p2->v[0] = 0.;
+    p2->v[1] = 0.;
+    p2->rho = 1.;
+    p2->P = 1.;
+    p1->a[0] = 0.;
+    p1->a[1] = 0.;
+    
+    
+    double dn[2] = {0.5 , 0};
+    
+    domain.update_gradients(dn, p1, p2);
+    
+    double res = (10 * (-3 * 0.5 + 2.25 * 1/4) / (7 * M_PI));
+    
+    BOOST_TEST(2 * res == p1->a[0], tt::tolerance(0.0001));
+    BOOST_TEST(res == p1->D, tt::tolerance(0.0001));
+    BOOST_TEST(0 == p1->a[1], tt::tolerance(0.0001));
+    
+    std::cout << p1->D << " " << p1->a[0] << std::endl;
+    
+    std::cout << 2 * (10 * (-3 * 0.5 + 2.25 * 1/4) / (7 * M_PI)) << std::endl;
+    
+    delete p1;
+    delete p2;
+    
+    // Need to check a and D
+}
+
+BOOST_AUTO_TEST_CASE(TestGradientGreater, * description("Distance between 1 and 2"))
+{
+    SPH_particle* p1 = new SPH_particle();
+    SPH_particle* p2 = new SPH_particle();
+    
+    domain.h = 1.;
+    domain.mass = 1.;
+    domain.mu = 1.;
+    
+    p1->v[0] = 1.;
+    p1->v[1] = 0.;
+    p1->rho = 1.;
+    p1->P = 1.;
+    p1->a[0] = 0.;
+    p1->a[1] = 0.;
+    p1->D = 0.;
+    
+    p2->v[0] = 0.;
+    p2->v[1] = 0.;
+    p2->rho = 1.;
+    p2->P = 1.;
+    p1->a[0] = 0.;
+    p1->a[1] = 0.;
+    
+    
+    double dn[2] = {1 , 0};
+    
+    domain.update_gradients(dn, p1, p2);
+    
+    double res = -10 * 0.75 / (7 * M_PI);
+    
+    BOOST_TEST(2 * res == p1->a[0], tt::tolerance(0.0001));
+    BOOST_TEST(res == p1->D, tt::tolerance(0.0001));
+    BOOST_TEST(0 == p1->a[1], tt::tolerance(0.0001));
+    
+    std::cout << p1->D << " " << p1->a[0] << std::endl;
+    
+    std::cout << 2 * (-10 * 0.75 / (7 * M_PI)) << std::endl;
+    
+    
+    
+    // Need to check a and D
+}
+
+BOOST_AUTO_TEST_CASE(TestGradinetOutside, * description("Distance outside the spline"))
+{
+    SPH_particle* p1 = new SPH_particle();
+    SPH_particle* p2 = new SPH_particle();
+    
+    domain.h = 1.;
+    domain.mass = 1.;
+    domain.mu = 1.;
+    
+    p1->v[0] = 1.;
+    p1->v[1] = 0.;
+    p1->rho = 1.;
+    p1->P = 1.;
+    p1->a[0] = 0.;
+    p1->a[1] = 0.;
+    p1->D = 0.;
+    
+    p2->v[0] = 0.;
+    p2->v[1] = 0.;
+    p2->rho = 1.;
+    p2->P = 1.;
+    p1->a[0] = 0.;
+    p1->a[1] = 0.;
+    
+    
+    double dn[2] = {0.5 , 0};
+    
+    domain.update_gradients(dn, p1, p2);
+    
+    double res = (10 * (-3 * 0.5 + 2.25 * 1/4) / (7 * M_PI));
+    
+    BOOST_TEST(2 * res == p1->a[0], tt::tolerance(0.0001));
+    BOOST_TEST(res == p1->D, tt::tolerance(0.0001));
+    BOOST_TEST(0 == p1->a[1], tt::tolerance(0.0001));
+    
+    std::cout << p1->D << " " << p1->a[0] << std::endl;
+    
+    std::cout << 2 * (10 * (-3 * 0.5 + 2.25 * 1/4) / (7 * M_PI)) << std::endl;
+    
+    
+    
+    // Need to check a and D
+}
+
+BOOST_AUTO_TEST_SUITE_END();
